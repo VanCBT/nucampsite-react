c2566180363d1ec0939907ef6fb226fb1c0a4635
Vanessa BT <vanessa@pilyr.io>
2024-08-29 01:27:34 -0400

    CampsitesDirectoryPage and CampsiteDetail

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage.js';
+import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 import Header from './components/Header.js';
 import Footer from './components/Footer.js';
 
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <Header />
       <CampsitesDirectoryPage />
       <Footer />
--- src/features/campsites/CampsitesList.js
+++ src/features/campsites/CampsitesList.js
@@ -1,6 +1,6 @@
 import { Col, Row } from 'reactstrap';
 import CampsiteCard from "./CampsiteCard";
-import { selectAllCompanies } from './campsitesSlice';
+import { selectAllCampsites } from './campsitesSlice';
 
 const CampsitesList = () => {
     const campsites = selectAllCampsites();
