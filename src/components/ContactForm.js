6779d8a06b8c471881c1b3c9c7be34c3c0ab264c
Vanessa BT <vanessa@pilyr.io>
2024-09-10 02:26:06 +0200

    ContactForm Part 3

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,10 +1,19 @@
 import { Button, Label, Col, FormGroup } from 'reactstrap';
 import { Formik, Field, Form } from 'formik';
 
+
+
 const ContactForm = () => {
+    const handleSubmit = (values, {resetForm}) => {
+        console.log('form values:', values);
+        console.log('in JSON format:', JSON.stringify(values));
+        resetForm();
+    };
+    
 
     return (
         <Formik
+            onSubmit={handleSubmit}
             initialValues={{
                 firstName: '',
                 lastName: '',
@@ -73,6 +82,11 @@ const ContactForm = () => {
                     </Col>
                 </FormGroup>
                 <FormGroup row> 
+                    <Col md={{ size: 10, offset: 2 }}>
+                        <Button type='submit' color='primary'>
+                                Send Feedback
+                        </Button>
+                    </Col>
                 </FormGroup>
             </Form>
         </Formik>
